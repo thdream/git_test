76c7f7ad10acd1ec88e9c26e64e828c6f1d407f8
lisi <lisi@163.com>
2020-03-28 17:49:47 +0800

    将项目提交到本地版本库

---
+++ git_test_project/src/com/ygsoft/git/GitTest.java
@@ -0,0 +1,19 @@
+package com.ygsoft.git;
+
+/**
+ * @author th
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git测试了");
+        System.out.println("git测试了");
+        System.out.println("git测试了");
+        System.out.println("git测试了");
+        System.out.println("git测试了");
+        System.out.println("git测试了");
+        System.out.println("git测试了");
+        System.out.println("git测试了");
+        System.out.println("git测试了");
+    }
+}
